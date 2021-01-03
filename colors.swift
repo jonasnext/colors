54a63ed3a5f76c43115d36f703770bb16bbbaa68
jonas <jonasnext@users.noreply.github.com>
2021-01-03 21:26:23 +0100

    swift p3 colors

--- colors.swift
+++ colors.swift
@@ -1,5 +1,115 @@
 // apple watch colors
 
+import SwiftUI
+
+// dark titanium
+// light titanium
+// ceramic
+// gold
+// navy blue
+// ivory
+
+// pale pink
+// white
+// black
+// evergreen
+// mango
+// slate
+
+// warm gray
+// watermelon
+// red
+// orange
+// clementine
+// apricot
+
+// peach
+// papaya
+// kumquat
+// light orange
+// mellow yellow
+// lemon cream
+
+// yellow
+// pollen
+// canary yellow
+// flash light
+// flash
+// green
+
+// spearmint
+// mint
+// beryl
+// sea foam
+// turquoise
+// light blue
+
+// surf blue
+// blue
+// royal blue
+// cerulean
+// lilac
+// mist blue
+
+// azure
+// northern blue
+// storm gray
+// cactus
+// pacific green
+// blue cobalt
+
+// alaskan blue
+// dark teal
+// blue horizon
+// deep navy
+// linen blue
+// denim blue
+
+// midnight blue
+// ocean blue
+// delft blue
+// indigo
+// purple
+// ultra violet
+
+// lavender gray
+// lavender
+// pink sand
+// light pink
+// vintage rose
+// grapefruit
+
+// pink
+// electric pink
+// pink citrus
+// neon pink
+// dragon fruit
+// hibiscus
+
+// pomegranate
+// plum
+// rose red
+// camellia
+// flamingo
+// camel
+
+// walnut
+// stone
+// antique white
+// cream
+// soft white
+// pebble
+
+// cocoa
+// coastal gray
+// dark olive
+// khaki
+// pine green
+// cyprus green
+
+// rose gold
+// yellow gold
+
 //
 // p3
 //
@@ -112,120 +222,116 @@
 // 225 168 137
 // 205 168 130
 
-
 //
 // srgb
 //
 
-//
+// …
 
-//
-//
+let darktitanium = Color(.displayP3, red: 84, green: 78, blue: 66, opacity: 1)
+let lighttitanium = Color(.displayP3, red: 191, green: 174, blue: 152, opacity: 1)
+let ceramic = Color(.displayP3, red: 244, green: 244, blue: 244, opacity: 1)
+let gold = Color(.displayP3, red: 181, green: 146, blue: 104, opacity: 1)
+let navyblue = Color(.displayP3, red: 33, green: 67, blue: 92, opacity: 1)
+let ivory = Color(.displayP3, red: 231, green: 226, blue: 218, opacity: 1)
 
-// dark titanium
-// light titanium
-// ceramic
-// gold
-// navy blue
-// ivory
+let palepink = Color(.displayP3, red: 234, green: 213, blue: 206, opacity: 1)
+let white = Color(.displayP3, red: 242, green: 242, blue: 242, opacity: 1)
+let black = Color(.displayP3, red: 26, green: 26, blue: 26, opacity: 1)
+let evergreen = Color(.displayP3, red: 88, green: 95, blue: 75, opacity: 1)
+let mango = Color(.displayP3, red: 245, green: 189, blue: 102, opacity: 1)
+let slate = Color(.displayP3, red: 56, green: 56, blue: 54, opacity: 1)
 
-// pale pink
-// white
-// black
-// evergreen
-// mango
-// slate
+let warmgray = Color(.displayP3, red: 188, green: 179, blue: 168, opacity: 1)
+let watermelon = Color(.displayP3, red: 184, green: 73, blue: 68, opacity: 1)
+let red = Color(.displayP3, red: 208, green: 61, blue: 57, opacity: 1)
+let orange = Color(.displayP3, red: 236, green: 110, blue: 48, opacity: 1)
+let clementine = Color(.displayP3, red: 236, green: 113, blue: 83, opacity: 1)
+let apricot = Color(.displayP3, red: 234, green: 124, blue: 89, opacity: 1)
 
-// warm gray
-// watermelon
-// red
-// orange
-// clementine
-// apricot
+let peach = Color(.displayP3, red: 213, green: 131, blue: 101, opacity: 1)
+let papaya = Color(.displayP3, red: 239, green: 138, blue: 80, opacity: 1)
+let kumquat = Color(.displayP3, red: 237, green: 142, blue: 92, opacity: 1)
+let lightorange = Color(.displayP3, red: 239, green: 153, blue: 54, opacity: 1)
+let mellowyellow = Color(.displayP3, red: 245, green: 243, blue: 192, opacity: 1)
+let lemoncream = Color(.displayP3, red: 251, green: 234, blue: 140, opacity: 1)
 
-// peach
-// papaya
-// kumquat
-// light orange
-// mellow yellow
-// lemon cream
+let yellow = Color(.displayP3, red: 226, green: 204, blue: 70, opacity: 1)
+let pollen = Color(.displayP3, red: 246, green: 210, blue: 110, opacity: 1)
+let canaryyellow = Color(.displayP3, red: 249, green: 223, blue: 98, opacity: 1)
+let flashlight = Color(.displayP3, red: 244, green: 236, blue: 77, opacity: 1)
+let flash = Color(.displayP3, red: 228, green: 241, blue: 85, opacity: 1)
+let green = Color(.displayP3, red: 159, green: 223, blue: 80, opacity: 1)
 
-// yellow
-// pollen
-// canary yellow
-// flash light
-// flash
-// green
+let spearmint = Color(.displayP3, red: 156, green: 230, blue: 149, opacity: 1)
+let mint = Color(.displayP3, red: 186, green: 234, blue: 164, opacity: 1)
+let beryl = Color(.displayP3, red: 217, green: 241, blue: 193, opacity: 1)
+let seafoam = Color(.displayP3, red: 217, green: 241, blue: 221, opacity: 1)
+let turquoise = Color(.displayP3, red: 169, green: 211, blue: 205, opacity: 1)
+let lightblue = Color(.displayP3, red: 127, green: 193, blue: 217, opacity: 1)
 
-// spearmint
-// mint
-// beryl
-// sea foam
-// turquoise
-// light blue
+let surfblue = Color(.displayP3, red: 62, green: 130, blue: 179, opacity: 1)
+let blue = Color(.displayP3, red: 85, green: 178, blue: 244, opacity: 1)
+let royalblue = Color(.displayP3, red: 117, green: 170, blue: 231, opacity: 1)
+let cerulean = Color(.displayP3, red: 158, green: 195, blue: 228, opacity: 1)
+let lilac = Color(.displayP3, red: 179, green: 164, blue: 219, opacity: 1)
+let mistblue = Color(.displayP3, red: 179, green: 184, blue: 168, opacity: 1)
 
-// surf blue
-// blue
-// royal blue
-// cerulean
-// lilac
-// mist blue
+let azure = Color(.displayP3, red: 137, green: 152, blue: 159, opacity: 1)
+let northernblue = Color(.displayP3, red: 122, green: 154, blue: 155, opacity: 1)
+let stormgray = Color(.displayP3, red: 111, green: 149, blue: 150, opacity: 1)
+let cactus = Color(.displayP3, red: 113, green: 136, blue: 129, opacity: 1)
+let pacificgreen = Color(.displayP3, red: 55, green: 124, blue: 153, opacity: 1)
+let bluecobalt = Color(.displayP3, red: 84, green: 123, blue: 154, opacity: 1)
 
-// azure
-// northern blue
-// storm gray
-// cactus
-// pacific green
-// blue cobalt
+let alaskanblue = Color(.displayP3, red: 100, green: 108, blue: 130, opacity: 1)
+let darkteal = Color(.displayP3, red: 67, green: 113, blue: 143, opacity: 1)
+let bluehorizon = Color(.displayP3, red: 53, green: 101, blue: 153, opacity: 1)
+let deepnavy = Color(.displayP3, red: 96, green: 112, blue: 149, opacity: 1)
+let linenblue = Color(.displayP3, red: 90, green: 106, blue: 145, opacity: 1)
+let denimblue = Color(.displayP3, red: 99, green: 130, blue: 164, opacity: 1)
 
-// alaskan blue
-// dark teal
-// blue horizon
-// deep navy
-// linen blue
-// denim blue
+let midnightblue = Color(.displayP3, red: 69, green: 77, blue: 114, opacity: 1)
+let oceanblue = Color(.displayP3, red: 118, green: 135, blue: 193, opacity: 1)
+let delftblue = Color(.displayP3, red: 94, green: 117, blue: 185, opacity: 1)
+let indigo = Color(.displayP3, red: 90, green: 98, blue: 159, opacity: 1)
+let purple = Color(.displayP3, red: 147, green: 125, blue: 239, opacity: 1)
+let ultraviolet = Color(.displayP3, red: 109, green: 87, blue: 183, opacity: 1)
 
-// midnight blue
-// ocean blue
-// delft blue
-// indigo
-// purple
-// ultra violet
+let lavendergray = Color(.displayP3, red: 134, green: 133, blue: 161, opacity: 1)
+let lavender = Color(.displayP3, red: 174, green: 155, blue: 165, opacity: 1)
+let pinksand = Color(.displayP3, red: 244, green: 195, blue: 172, opacity: 1)
+let lightpink = Color(.displayP3, red: 233, green: 178, blue: 172, opacity: 1)
+let vintagerose = Color(.displayP3, red: 233, green: 174, blue: 166, opacity: 1)
+let grapefruit = Color(.displayP3, red: 242, green: 180, blue: 145, opacity: 1)
 
-// lavender gray
-// lavender
-// pink sand
-// light pink
-// vintage rose
-// grapefruit
+let pink = Color(.displayP3, red: 235, green: 101, blue: 104, opacity: 1)
+let electricpink = Color(.displayP3, red: 234, green: 91, blue: 88, opacity: 1)
+let pinkcitrus = Color(.displayP3, red: 221, green: 91, blue: 79, opacity: 1)
+let neonpink = Color(.displayP3, red: 233, green: 77, blue: 68, opacity: 1)
+let dragonfruit = Color(.displayP3, red: 224, green: 87, blue: 126, opacity: 1)
+let hibiscus = Color(.displayP3, red: 231, green: 60, blue: 92, opacity: 1)
 
-// pink
-// electric pink
-// pink citrus
-// neon pink
-// dragon fruit
-// hibiscus
+let pomegranate = Color(.displayP3, red: 206, green: 60, blue: 95, opacity: 1)
+let plum = Color(.displayP3, red: 137, green: 70, blue: 76, opacity: 1)
+let rosered = Color(.displayP3, red: 169, green: 54, blue: 71, opacity: 1)
+let camellia = Color(.displayP3, red: 185, green: 78, blue: 72, opacity: 1)
+let flamingo = Color(.displayP3, red: 198, green: 134, blue: 112, opacity: 1)
+let camel = Color(.displayP3, red: 175, green: 144, blue: 103, opacity: 1)
 
-// pomegranate
-// plum
-// rose red
-// camellia
-// flamingo
-// camel
+let walnut = Color(.displayP3, red: 168, green: 135, blue: 104, opacity: 1)
+let stone = Color(.displayP3, red: 171, green: 153, blue: 131, opacity: 1)
+let antiquewhite = Color(.displayP3, red: 208, green: 183, blue: 151, opacity: 1)
+let cream = Color(.displayP3, red: 249, green: 228, blue: 187, opacity: 1)
+let softwhite = Color(.displayP3, red: 219, green: 209, blue: 188, opacity: 1)
+let pebble = Color(.displayP3, red: 170, green: 156, blue: 143, opacity: 1)
 
-// walnut
-// stone
-// antique white
-// cream
-// soft white
-// pebble
+let cocoa = Color(.displayP3, red: 152, green: 142, blue: 139, opacity: 1)
+let coastalgray = Color(.displayP3, red: 126, green: 110, blue: 98, opacity: 1)
+let darkolive = Color(.displayP3, red: 140, green: 140, blue: 120, opacity: 1)
+let khaki = Color(.displayP3, red: 137, green: 126, blue: 92, opacity: 1)
+let pinegreen = Color(.displayP3, red: 98, green: 124, blue: 103, opacity: 1)
+let cyprusgreen = Color(.displayP3, red: 100, green: 110, blue: 90, opacity: 1)
 
-// cocoa
-// coastal gray
-// dark olive
-// khaki
-// pine green
-// cyprus green
-
-// rose gold
-// yellow gold
+let rosegold = Color(.displayP3, red: 225, green: 168, blue: 137, opacity: 1)
+let yellowgold = Color(.displayP3, red: 205, green: 168, blue: 130, opacity: 1)
