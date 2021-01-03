e3308b1d473908a1b39c678fd2994c6d42877f71
jonas <jonasnext@users.noreply.github.com>
2021-01-03 21:02:08 +0100

    p3 colors

---
+++ colors.swift
@@ -0,0 +1,231 @@
+// apple watch colors
+
+//
+// p3
+//
+
+// 84 78 66
+// 191 174 152
+// 244 244 244
+// 181 146 104
+// 33 67 92
+// 231 226 218
+
+// 234 213 206
+// 242 242 242
+// 26 26 26
+// 88 95 75
+// 245 189 102
+// 56 56 54
+
+// 188 179 168
+// 184 73 68
+// 208 61 57
+// 236 110 48
+// 236 113 83
+// 234 124 89
+
+// 213 131 101
+// 239 138 80
+// 237 142 92
+// 239 153 54
+// 245 243 192
+// 251 234 140
+
+// 226 204 70
+// 246 210 110
+// 249 223 98
+// 244 236 77
+// 228 241 85
+// 159 223 80
+
+// 156 230 149
+// 186 234 164
+// 217 241 193
+// 217 241 221
+// 169 211 205
+// 127 193 217
+
+// 62 130 179
+// 85 178 244
+// 117 170 231
+// 158 195 228
+// 179 164 219
+// 179 184 168
+
+// 137 152 159
+// 122 154 155
+// 111 149 150
+// 113 136 129
+// 55 124 153
+// 84 123 154
+
+// 100 108 130
+// 67 113 143
+// 53 101 153
+// 96 112 149
+// 90 106 145
+// 99 130 164
+
+// 69 77 114
+// 118 135 193
+// 94 117 185
+// 90 98 159
+// 147 125 239
+// 109 87 183
+
+// 134 133 161
+// 174 155 165
+// 244 195 172
+// 233 178 172
+// 233 174 166
+// 242 180 145
+
+// 235 101 104
+// 234 91 88
+// 221 91 79
+// 233 77 68
+// 224 87 126
+// 231 60 92
+
+// 206 60 95
+// 137 70 76
+// 169 54 71
+// 185 78 72
+// 198 134 112
+// 175 144 103
+
+// 168 135 104
+// 171 153 131
+// 208 183 151
+// 249 228 187
+// 219 209 188
+// 170 156 143
+
+// 152 142 139
+// 126 110 98
+// 140 140 120
+// 137 126 92
+// 98 124 103
+// 100 110 90
+
+// 225 168 137
+// 205 168 130
+
+
+//
+// srgb
+//
+
+//
+
+//
+//
+
+// dark titanium
+// light titanium
+// ceramic
+// gold
+// navy blue
+// ivory
+
+// pale pink
+// white
+// black
+// evergreen
+// mango
+// slate
+
+// warm gray
+// watermelon
+// red
+// orange
+// clementine
+// apricot
+
+// peach
+// papaya
+// kumquat
+// light orange
+// mellow yellow
+// lemon cream
+
+// yellow
+// pollen
+// canary yellow
+// flash light
+// flash
+// green
+
+// spearmint
+// mint
+// beryl
+// sea foam
+// turquoise
+// light blue
+
+// surf blue
+// blue
+// royal blue
+// cerulean
+// lilac
+// mist blue
+
+// azure
+// northern blue
+// storm gray
+// cactus
+// pacific green
+// blue cobalt
+
+// alaskan blue
+// dark teal
+// blue horizon
+// deep navy
+// linen blue
+// denim blue
+
+// midnight blue
+// ocean blue
+// delft blue
+// indigo
+// purple
+// ultra violet
+
+// lavender gray
+// lavender
+// pink sand
+// light pink
+// vintage rose
+// grapefruit
+
+// pink
+// electric pink
+// pink citrus
+// neon pink
+// dragon fruit
+// hibiscus
+
+// pomegranate
+// plum
+// rose red
+// camellia
+// flamingo
+// camel
+
+// walnut
+// stone
+// antique white
+// cream
+// soft white
+// pebble
+
+// cocoa
+// coastal gray
+// dark olive
+// khaki
+// pine green
+// cyprus green
+
+// rose gold
+// yellow gold
